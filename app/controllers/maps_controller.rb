3fd8c73bc112b0239d821d0f250584f9f344f2a6
Erik Arrieta <yesiamokay@gmail.com>
2014-09-16 09:32:47 -0700

    Polished search box, began working on search (maps) controller logic

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,16 @@
 class MapsController < ApplicationController
   
   def show 
+    @origin = params['q']
+    @destination = params['r']
+    
+    if (@origin.nil? || @origin == "") 
+      @origin = "Unspecified"
+    end
+    if (@destination.nil? || @destination == "") 
+      @destination = "Unspecified"
+    end
+    
   end
   
 end
